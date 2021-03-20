b2ece8080827c9c825ac72d7744ee74e5fc6b66d
Akashi2019 <657318304@qq.com>
2021-03-20 22:12:23 +0800

    fix:Router match

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage'
-import ProductPage from './pages/ProductPage'
-import UserPage from './pages/UserPage'
-import _404Page from './pages/_404Page'
+import LoginPage from './pages/LoginPage';
+import ProductPage from './pages/ProductPage';
+import UserPage from './pages/UserPage';
+import _404Page from './pages/_404Page';
 
 import {
   BrowserRouter as Router,
   Route,
-  Link,
+  Link
   // Switch
 } from './react-router-dom';
 import './App.css';
@@ -22,11 +22,18 @@ function App() {
         <Link to="/login">登录</Link>
         <Link to="/product/123">商品</Link>
         {/* <Switch> */}
-          <Route path="/" exact component={HomePage}/>
-          <Route path="/product/:xx" exact component={ProductPage}/>
-          <Route path="/user" exact component={UserPage}/>
-          <Route path="/login" exact component={LoginPage}/>
-          {/* <Route component={_404Page}/> */}
+        <Route
+          path="/"
+          exact
+          // children={() => {
+          //   return <div>children</div>;
+          // }}
+          component={HomePage}
+        />
+        <Route path="/product/:xx" exact component={ProductPage} />
+        <Route path="/user" exact component={UserPage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route component={_404Page}/>
         {/* </Switch> */}
       </Router>
     </div>
--- src/react-router-dom/Route.js
+++ src/react-router-dom/Route.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { RouterContext } from './RouterContext';
+import matchPath from './matchPath';
 
 export default class Route extends Component {
   render() {
@@ -7,9 +8,27 @@ export default class Route extends Component {
       <RouterContext.Consumer>
         {(context) => {
           const { location } = context;
-          const { path, component } = this.props;
-          const match = location.pathname === path;
-          return match ? React.createElement(component) : null;
+          const { path, children, component, render } = this.props;
+          const match = path
+            ? matchPath(location.pathname, this.props)
+            : context.match;
+          const props = {
+            ...context,
+            match
+          };
+          return match
+            ? children
+              ? typeof children === 'function'
+                ? children(props)
+                : children
+              : component
+              ? React.createElement(component, props)
+              : render
+              ? render(props)
+              : null
+            : typeof children === 'function'
+            ? children(props)
+            : null;
         }}
       </RouterContext.Consumer>
     );
